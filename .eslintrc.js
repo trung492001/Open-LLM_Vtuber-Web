f2f3a0954d91f87d665dc1d45f6aae453ed4e6fe
ylxmf <ethanelift@gmail.com>
2024-12-14 00:55:02 +0800

    chore: update project configuration and dependencies
    - Added Docusaurus scripts to package.json for documentation
      development and building.
    - Updated .gitignore to include additional build and temp files.
    - Added new dependencies for Docusaurus and ESLint plugins.
    - Enabled incremental builds in TypeScript configuration files.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended'
+    ],
+    plugins: ['@typescript-eslint', 'react'],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    },
+    rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never'
+            }
+        ]
+    }
+};
